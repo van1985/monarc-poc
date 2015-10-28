1f170128f978b846e0f55861c8ec52f8012b3aba
Leandro García Vandenbosch <vanden@Leandros-MacBook-Pro.local>
2015-10-28 15:53:05 -0300

    Initial Structure

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name monarcPocApp
+ * @description
+ * # monarcPocApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('monarcPocApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monarcPocApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monarcPocApp
+ */
+angular.module('monarcPocApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
