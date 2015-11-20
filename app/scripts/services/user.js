18908e2b3afcdea0bc494fd1447aaba31d123966
fbonecco <fbonecco@gmail.com>
2015-11-20 12:36:21 -0300

    Few changes

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -6,10 +6,14 @@ angular.module('monarcPocApp').service('UserSrv', function() {
       username=null;
 
   service.getUser = function(){
+  	if ( (localStorage.getItem("user") !== null) || (localStorage.getItem("user") !== undefined)){
       return this.username;
+  	}
+  	return null;
   };
 
   service.setUser = function(username) {
+  	localStorage.setItem("user", username);
     this.username = username;
   };
 
